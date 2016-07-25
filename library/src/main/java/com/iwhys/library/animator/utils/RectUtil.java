0211689ee3fd9ea7e829ed7243e0656b4fc9e8f2
devil <iwhs@qq.com>
2016-07-25 18:18:09 +0800

    add rect utils

--- library/src/main/java/com/iwhys/library/animator/utils/RectUtil.java
+++ library/src/main/java/com/iwhys/library/animator/utils/RectUtil.java
@@ -2,6 +2,7 @@ package com.iwhys.library.animator.utils;
 
 import android.graphics.Rect;
 import android.graphics.RectF;
+import android.support.annotation.FloatRange;
 import android.support.annotation.NonNull;
 
 /**
@@ -31,4 +32,57 @@ public class RectUtil {
     public static void centerTo(@NonNull RectF rectF, float toX, float toY){
         rectF.offset(toX - rectF.centerX(), toY - rectF.centerY());
     }
+
+    /**
+     * Scale the rect
+     * @param rect the rect to scale
+     * @param factor the scale factor
+     * @param pivotX the pivotX[0, 1]
+     * @param pivotY the pivotY[0, 1]
+     */
+    public static void scale(@NonNull Rect rect, float factor, @FloatRange(from = 0, to = 1f) float pivotX, @FloatRange(from = 0, to = 1f) float pivotY){
+        int newWidth = (int) (rect.width() * factor);
+        int newHeight = (int) (rect.height() * factor);
+        float px = rect.left + rect.width() * pivotX;
+        float py = rect.top + rect.height() * pivotY;
+        int newLeft = (int) (px - newWidth * pivotX);
+        int newTop = (int) (py - newHeight * pivotY);
+        rect.set(newLeft, newTop, newLeft + newWidth, newTop + newHeight);
+    }
+
+    /**
+     * Scale the rect
+     * @param rect the rect to scale
+     * @param factor the scale factor
+     */
+    public static void scale(@NonNull Rect rect, float factor){
+        scale(rect, factor, 0.5f, 0.5f);
+    }
+
+    /**
+     * Scale the rectF
+     * @param rect the rect to scale
+     * @param factor the scale factor
+     * @param pivotX the pivotX[0, 1]
+     * @param pivotY the pivotY[0, 1]
+     */
+    public static void scale(@NonNull RectF rect, float factor, @FloatRange(from = 0, to = 1f) float pivotX, @FloatRange(from = 0, to = 1f) float pivotY){
+        float newWidth = rect.width() * factor;
+        float newHeight = rect.height() * factor;
+        float px = rect.left + rect.width() * pivotX;
+        float py = rect.top + rect.height() * pivotY;
+        float newLeft = px - newWidth * pivotX;
+        float newTop = py - newHeight * pivotY;
+        rect.set(newLeft, newTop, newLeft + newWidth, newTop + newHeight);
+    }
+
+    /**
+     * Scale the rect
+     * @param rect the rect to scale
+     * @param factor the scale factor
+     */
+    public static void scale(@NonNull RectF rect, float factor){
+        scale(rect, factor, 0.5f, 0.5f);
+    }
+
 }
